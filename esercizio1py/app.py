6e14c7db3d5885d8c21e796f6530cd946667653c
Gabbo La Torre <la.torre.gabriele@iisgalvanimi.edu.it>
2024-12-02 11:41:09 +0000

    primo commit

--- esercizio1py/app.py
+++ esercizio1py/app.py
@@ -8,8 +8,20 @@ lista_spesa = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', lista = lista_spesa)
 
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
