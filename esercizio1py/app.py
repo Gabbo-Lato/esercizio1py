7bae9744d080d7deebc720902d1db3d2d636def7
Gabbo La Torre <la.torre.gabriele@iisgalvanimi.edu.it>
2024-12-04 09:25:13 +0000

    aggiunto flask-sql

--- esercizio1py/app.py
+++ esercizio1py/app.py
@@ -1,10 +1,18 @@
 
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 
 #inizializza l'app Flask
 app = Flask(__name__)
 lista_spesa = []
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+with app.app_context():
+db.create_all()
+
+
 
 @app.route('/')
 def home():
