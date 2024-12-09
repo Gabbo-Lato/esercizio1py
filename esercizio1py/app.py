8481ce0644055c7387db467582b6f5634fb3b001
Gabbo La Torre <la.torre.gabriele@iisgalvanimi.edu.it>
2024-12-09 11:19:30 +0000

    commit finale

--- esercizio1py/app.py
+++ esercizio1py/app.py
@@ -1,34 +1,39 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from models import db, ListaSpesa
 
 #inizializza l'app Flask
 app = Flask(__name__)
-lista_spesa = []
+
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 with app.app_context():
-db.create_all()
-
+    db.create_all()
 
 
 @app.route('/')
 def home():
+    lista_spesa = ListaSpesa.query.all()
     return render_template('index.html', lista = lista_spesa)
 
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
     elemento = request.form['elemento']
     if elemento:
-        lista_spesa.append(elemento)
+        nuovo_elemento = ListaSpesa(elemento=elemento) 
+        db.session.add(nuovo_elemento) 
+        db.session.commit() 
     return redirect(url_for('home'))
 
 @app.route('/rimuovi/<int:indice>', methods=['POST'])
 def rimuovi(indice):
-    if 0 <= indice < len(lista_spesa):
-        lista_spesa.pop(indice)
+    elemento = ListaSpesa.query.get_or_404(indice) 
+    db.session.delete(elemento) 
+    db.session.commit() 
     return redirect(url_for('home'))
 
 #avvio dell'app Flask
--- esercizio1py/models.py
+++ esercizio1py/models.py
@@ -2,6 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
+
 class ListaSpesa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     elemento = db.Column(db.String(100), nullable=False)
